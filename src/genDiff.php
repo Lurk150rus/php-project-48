2e6ae22cd20f8c0558d13263e71747e98c5367cc
Sasyk Kirill Vasilevich <sasyk@equip.me>
2025-12-30 15:09:51 +0300

    genDiff test

--- src/genDiff.php
+++ src/genDiff.php
@@ -2,12 +2,15 @@
 
 namespace Hexlet\Code;
 
-function genDiff($firstFilePath, $secondFilePath)
+function genDiff($firstFilePath, $secondFilePath): array
 {
-    $files = array_filter([$firstFilePath, $secondFilePath]);
+    $files = array_filter([trim($firstFilePath), trim($secondFilePath)]);
+    
     if (count($files) !== 2) {
         throw new \Exception("Invalid number of files");
     }
+
+    [$firstFilePath, $secondFilePath] = $files;
 
     $firstFileData = (new FileParser($firstFilePath))->getParsedData();
     $secondFileData = (new FileParser($secondFilePath))->getParsedData();
---
+++ tests/genDiffTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hexlet\Code\Tests;
+
+use Exception;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+use function Hexlet\Code\genDiff;
+
+final class genDiffTest extends TestCase
+{
+    private $dirpath;
+
+    public function setUp(): void
+    {
+        $this->dirpath = __DIR__ . '/fixtures/';
+    }
+
+    public function testCorrect(): void
+    {
+        $correctDiff = array(
+            ' - follow' => false,
+            'host' => 'hexlet.io',
+            ' - proxy' => '123.234.53.22',
+            ' + timeout' => 50,
+            ' - timeout' => 20,
+            ' + verbose' => true,
+        );
+
+        $filePath1 = $this->dirpath . 'file1.json';
+        $filePath2 = $this->dirpath . 'file2.json';
+
+        $diff_data = genDiff($filePath1, $filePath2);
+
+        $this->assertEquals($diff_data, $correctDiff);
+    }
+
+    #[DataProvider('incorrectFilesDataProvider')]
+    public function testIncorrectFiles(...$files): void
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Invalid number of files');
+        genDiff(...$files);
+    }
+
+    public static function incorrectFilesDataProvider(): array
+    {
+        return [
+            'empty file' => ['', ''],
+            'only spaces' => ['   ', '   '],
+        ];
+    }
+}
