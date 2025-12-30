b54313b3bc6d41c967f2507657cc1a3b25eb4a2e
Sasyk Kirill Vasilevich <sasyk@equip.me>
2025-12-30 18:51:58 +0300

    Symfony Yaml + abstract class

---
+++ src/Parsers/AbstractParser.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hexlet\Code\Parsers;
+
+abstract class AbstractParser implements ParserInterface
+{
+    protected string $path;
+    protected string $fileContent;
+    protected array $parsedData = [];
+
+    public function __construct(string $path)
+    {
+        $this->path = $path;
+        $this->loadContent();
+        $this->parse();
+    }
+
+    protected function loadContent(): void
+    {
+        if (!file_exists($this->path)) {
+            throw new \RuntimeException('File not found');
+        }
+
+        $this->fileContent = (string) file_get_contents($this->path);
+    }
+
+    abstract protected function parse(): void;
+
+    public function getParsedData(): array
+    {
+        return $this->parsedData;
+    }
+}
--- src/Parsers/JsonParser.php
+++ src/Parsers/JsonParser.php
@@ -4,61 +4,16 @@ declare(strict_types=1);
 
 namespace Hexlet\Code\Parsers;
 
-final class JsonParser implements ParserInterface
+final class JsonParser extends AbstractParser
 {
-    /**
-     * Путь к файлу
-     * @var string
-     */
-    private string $path;
-    /**
-     * Строчное представление, содержимого файла
-     * @var string
-     */
-    private string $file_content;
-    /**
-     * Данные, в формате массива
-     * @var array
-     */
-    private array $parsed_data;
-
-    /**
-     * @inheritDoc
-     */
-    public function __construct(string $path)
-    {
-        $this->path = $path;
-        $this->getContent();
-        $this->parse();
-    }
-    /**
-     * Summary of getContent
-     * @throws \Exception
-     * @return void
-     */
-    private function getContent(): void
-    {
-        if (!file_exists($this->path)) {
-            throw new \Exception("File not found");
-        }
-        $this->file_content = (string) file_get_contents($this->path);
-    }
 
     /**
      * Summary of parse
      * @return void
      */
-    private function parse(): void
+    protected function parse(): void
     {
-        $this->parsed_data = json_decode($this->file_content, true) ?? [];
+        $this->parsedData = json_decode($this->fileContent, true) ?? [];
     }
 
-    /**
-     * Summary of getParsedData
-     * @return array
-     */
-    public function getParsedData(): array
-    {
-        return $this->parsed_data;
-    }
 }
